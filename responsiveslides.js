c2eb1f5a1f649e413d98a7d8b0e4c37aaaf0889f
Viljami S <viljami.salminen@gmail.com>
2012-12-14 19:08:32 +0200

    Consistency fixes

--- responsiveslides.js
+++ responsiveslides.js
@@ -69,16 +69,16 @@
         supportsTransitions = (function () {
           var b = document.body || document.documentElement;
           var s = b.style;
-          var p = 'transition';
-          if (typeof s[p] === 'string') {
+          var p = "transition";
+          if (typeof s[p] === "string") {
             return true;
           }
           // Tests for vendor specific prop
-          v = ['Moz', 'Webkit', 'Khtml', 'O', 'ms'];
+          v = ["Moz", "Webkit", "Khtml", "O", "ms"];
           p = p.charAt(0).toUpperCase() + p.substr(1);
           var i;
           for (i = 0; i < v.length; i++) {
-            if (typeof s[v[i] + p] === 'string') {
+            if (typeof s[v[i] + p] === "string") {
               return true;
             }
           }
