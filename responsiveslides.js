2ace946cecc6f7ee6bca068b34a7ed893f310807
Jacob Bennett <me@jakebennett.net>
2014-02-25 14:49:28 -0600

    feat: Added seperate containers for nav controls and pager controls

--- responsiveslides.js
+++ responsiveslides.js
@@ -24,7 +24,8 @@
       "prevText": "Previous",   // String: Text for the "previous" button
       "nextText": "Next",       // String: Text for the "next" button
       "maxwidth": "",           // Integer: Max-width of the slideshow, in pixels
-      "navContainer": "",       // Selector: Where auto generated controls should be appended to, default is after the <ul>
+      "pagerContainer": "",       // Selector: Where auto generated pager controls should be appended to, default is after the <ul>
+      "navContainer": "",       // Selector: Where auto generated prev next nav should be appended to, default is after the <ul>
       "manualControls": "",     // Selector: Declare custom pager navigation
       "namespace": "rslides",   // String: change the default namespace used
       "before": $.noop,         // Function: Before callback
@@ -191,8 +192,8 @@
           $pager.append(tabMarkup);
 
           // Inject pager
-          if (options.navContainer) {
-            $(settings.navContainer).append($pager);
+          if (options.pagerContainer) {
+            $(settings.pagerContainer).append($pager);
           } else {
             $this.after($pager);
           }
